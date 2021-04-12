c5ca039d81258ff6cc634374aa9744a9912214dc
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-04-12 17:16:58 -0500

    Matched style to sketch - added fonts to index.html set up theme - added makeStyles to navbar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -10,6 +10,20 @@ const useStyles = makeStyles((theme) => ({
   },
   link: {
     marginLeft: 24,
+    textDecoration: "none",
+    padding: 1,
+    color: theme.palette.common.black,
+  },
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: 12,
+    borderBottom: `1px solid ${theme.palette.primary.shadow}`,
+  },
+
+  active: {
+    marginTop: 2,
+    borderBottom: `2px solid ${theme.palette.primary.main}`,
   },
 }));
 
@@ -17,7 +31,7 @@ function NavBar() {
   const classes = useStyles();
 
   return (
-    <Box display="flex" justifyContent="space-between" padding="8px">
+    <Box className={classes.nav}>
       {/* Logo and title  */}
       <Box display="flex" id="nav-Left">
         <img src={logo} className={classes.logo} />
@@ -25,15 +39,17 @@ function NavBar() {
       </Box>
 
       {/* Links and login */}
-      <Box display="flex" id="nav-Right">
+      <Box display="flex" alignItems="center" id="nav-Right">
         <Link className={classes.link} to="explore">
-          EXPLORE
+          <Typography variant="subtitle1" className={classes.active}>
+            EXPLORE
+          </Typography>
         </Link>
-        <Link className={classes.link} to="explore">
-          LAUNCH
+        <Link className={classes.link} to="launch">
+          <Typography variant="subtitle1">LAUNCH</Typography>
         </Link>
-        <Link className={classes.link} to="explore">
-          LOGIN
+        <Link className={classes.link} to="login">
+          <Typography variant="subtitle1">LOGIN</Typography>
         </Link>
       </Box>
     </Box>
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,16 +2,20 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: "ProximaNova-Semibold",
+    fontFamily: ["Proxima Nova", "Roboto"].join(","),
     fontSize: 12,
     h6: {
-      fontFamily: "Gilroy",
-      fontWeight: 700,
+      fontFamily: "Gilroy-Medium",
       fontSize: 24,
       // could customize the h1 variant as well
     },
+    subtitle1: {
+      fontSize: 14,
+      fontWeight: 400,
+      lineHeight: 1,
+    },
   },
   palette: {
-    primary: { main: "#69E781" },
+    primary: { main: "#69E781", shadow: "#E8E8E8" },
   },
 });
