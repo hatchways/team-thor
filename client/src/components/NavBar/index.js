b62222f5232e298e19f2dec0419835700e192bff
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-04-12 16:00:35 -0500

    set up basics of nav bar started on theme and fonts

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,7 +1,43 @@
-import { Box } from "@material-ui/core";
+import { Box, makeStyles, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import logo from "./ic-logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    height: 34,
+    width: 34,
+    marginRight: 8,
+  },
+  link: {
+    marginLeft: 24,
+  },
+}));
 
 function NavBar() {
-  return <Box>Skeleton Nav Bar</Box>;
+  const classes = useStyles();
+
+  return (
+    <Box display="flex" justifyContent="space-between" padding="8px">
+      {/* Logo and title  */}
+      <Box display="flex" id="nav-Left">
+        <img src={logo} className={classes.logo} />
+        <Typography variant="h6">Product Launch</Typography>
+      </Box>
+
+      {/* Links and login */}
+      <Box display="flex" id="nav-Right">
+        <Link className={classes.link} to="explore">
+          EXPLORE
+        </Link>
+        <Link className={classes.link} to="explore">
+          LAUNCH
+        </Link>
+        <Link className={classes.link} to="explore">
+          LOGIN
+        </Link>
+      </Box>
+    </Box>
+  );
 }
 
 export default NavBar;
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,13 +2,16 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"Roboto"',
+    fontFamily: "ProximaNova-Semibold",
     fontSize: 12,
-    h1: {
+    h6: {
+      fontFamily: "Gilroy",
+      fontWeight: 700,
+      fontSize: 24,
       // could customize the h1 variant as well
-    }
+    },
   },
   palette: {
-    primary: { main: "#DF1B1B" }
-  }
+    primary: { main: "#69E781" },
+  },
 });
